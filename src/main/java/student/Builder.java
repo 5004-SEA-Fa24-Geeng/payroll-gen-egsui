668ae99f6fdc8695d1bd98eb0f1f4cb653580d6d
Yujing Wei <sylverwei@gmail.com>
2025-02-10 11:03:58 -0800

    Update files

--- src/main/java/student/Builder.java
+++ src/main/java/student/Builder.java
@@ -76,7 +76,7 @@ public final class Builder {
         }
         try {
             hours = Double.parseDouble(parts[1]);
-            if (hours > 0) {
+            if (hours >= 0) {
                 return new TimeCard(parts[0], hours);
             } else {
                 throw new IllegalArgumentException("worked hours should be greater than zero.");
